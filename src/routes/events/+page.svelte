99bff948b7f3dde6c2e394c768c2a93795909f50
Plyrs <me@plyrs.eu.org>
2023-01-16 12:20:41 +0700

    fix: navbar, team page grid, event banner click
    - navbar link click now doesn't trigger drawer open
    - get page url from stores instead of afterNavigate

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -8,7 +8,7 @@
   <div class="container grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-1 px-6">
     {#each data.eventData as eventData}
       <div class="hover:scale-110 hover:rotate-6 duration-300">
-        <a href={eventData.href}><img src={eventData.img} alt="event banner" /></a>
+        <a href={eventData.href} target="_blank" rel="noreferrer"><img src={eventData.img} alt="event banner" /></a>
       </div>
     {/each}
   </div>
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -150,24 +150,26 @@
   ]
 </script>
 
-<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 px-6">
-  {#each teamData as team}
-    <div class=" bg-kusogaki-purple rounded-md font-lemon-milk">
-      <div class="overflow-hidden w-full h-min">
-        <a href={team.profileUrl} target="_blank" rel="noreferrer">
-          <img class="rounded-t-md w-full hover:scale-110 hover:rotate-6 duration-300" src={team.pic} alt="" />
-        </a>
-      </div>
-      <div class="m-2">
-        <a href={team.profileUrl} target="_blank" rel="noreferrer">
-          <h5 class="text-sm sm:text-lg font-bold text-white my-2">{team.name}</h5>
-        </a>
-        <div class="flex flex-wrap gap-1 w-full">
-          {#each team.badges as badge}
-            <span class="{badgeLists[badge].color} text-gray-700 text-xs font-medium px-2.5 py-0.5 rounded">{badgeLists[badge].name}</span>
-          {/each}
+<div class="grid w-full place-items-center">
+  <div class="container grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 px-6">
+    {#each teamData as team}
+      <div class=" bg-kusogaki-purple rounded-md font-lemon-milk">
+        <div class="overflow-hidden w-full h-min">
+          <a href={team.profileUrl} target="_blank" rel="noreferrer">
+            <img class="rounded-t-md w-full hover:scale-110 hover:rotate-6 duration-300" src={team.pic} alt="" />
+          </a>
+        </div>
+        <div class="m-2">
+          <a href={team.profileUrl} target="_blank" rel="noreferrer">
+            <h5 class="text-sm sm:text-lg font-bold text-white my-2">{team.name}</h5>
+          </a>
+          <div class="flex flex-wrap gap-1 w-full">
+            {#each team.badges as badge}
+              <span class="{badgeLists[badge].color} text-gray-700 text-xs font-medium px-2.5 py-0.5 rounded">{badgeLists[badge].name}</span>
+            {/each}
+          </div>
         </div>
       </div>
-    </div>
-  {/each}
+    {/each}
+  </div>
 </div>
