4137000da95f6ca4d81e2241916010ca569fe158
Plyrs <me@plyrs.eu.org>
2023-01-19 03:27:49 +0700

    fix: adjust event banner z depth on hover

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -11,7 +11,7 @@
 <div class="grid w-full place-items-center">
   <div class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 px-6">
     {#each data.eventData as eventData}
-      <div class="hover:scale-110 hover:rotate-6 duration-300 w-full aspect-[3/1]">
+      <div class="hover:scale-110 hover:rotate-6 hover:z-10 duration-300 w-full aspect-[3/1]">
         <a href={eventData.href} target="_blank" rel="noreferrer"><ImageLoad src={eventData.img} alt="event banner" className="z-0" /></a>
       </div>
     {/each}
