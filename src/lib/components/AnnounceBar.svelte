00d1883cb964292f87be497a5796159e663e62f1
Plyrs <me@plyrs.eu.org>
2023-01-21 00:03:37 +0700

    feat: load announcement from api
    - add animation on hover
    - open in new tab

--- src/lib/components/AnnounceBar.svelte
+++ src/lib/components/AnnounceBar.svelte
@@ -4,14 +4,14 @@
 </script>
 
 <div
-  class="w-fit max-w-xs sm:max-w-sm md:max-w-md xl:max-w-lg 2xl:max-w-xl p-2 mb-4 {!isDark
+  class="max-w-xs sm:max-w-sm md:max-w-md xl:max-w-lg 2xl:max-w-xl p-2 rounded-l-full shadow right-0 hover:scale-110 duration-200 {!isDark
     ? 'bg-white'
-    : 'bg-kusogaki-purple'} rounded-l-full shadow right-0"
+    : 'bg-kusogaki-purple'}"
 >
   <div
-    class="ml-3 my-0 md:my-2 xl:my-4 text-xs md:text-sm xl:text-2xl font-lemon-milk {isDark
+    class="ml-3 my-0 md:my-2 xl:my-4 text-xs md:text-sm xl:text-2xl font-lemon-milk whitespace-nowrap text-ellipsis overflow-hidden right-0 {isDark
       ? 'text-white'
-      : ''} whitespace-nowrap text-ellipsis overflow-hidden right-0"
+      : ''}"
   >
     <slot />
     {message}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { fly } from 'svelte/transition'
+
   import AnnounceBar from '$src/lib/components/AnnounceBar.svelte'
   import Meta from '$src/lib/components/Meta.svelte'
 
-  import type { PageData } from './$types'
-  export let data: PageData
+  import type { AnnouncementData } from './home'
+  export let announcementData: Array<AnnouncementData> = []
+  let isDataLoaded = false
+  async function fetchData() {
+    if (isDataLoaded) return
+    try {
+      const response = await fetch(`https://kusogaki-backend.plyrs.party/api/collections/events/records?sort=-startDate&filter=(isAnnounced=true)`)
+      const data = await response.json()
+      announcementData = data.items
+      isDataLoaded = true
+    } catch (error) {
+      isDataLoaded = false
+      console.error(error)
+    }
+  }
+  onMount(fetchData)
 </script>
 
 <Meta
   title="Home"
   description="This is just a placeholder so the embed looks nice and full. I don't know what else to write so I'll just tell you a story about a chicken. Once upon a time, there's a mom chicken that laid an egg. Mom chicken takes care of the egg everyday. She keep it warm, giving motherly love to it since the first time it came out to the world. But in an unfortunate day, an evil human came to mom chicken and took the egg she loved and raised with utmost care. Mom chicken became sad, she cried everyday, until she got fed up of this cruel life and walked to the nearest KFC to give up her life."
 />
-<div class="fixed grid right-0 justify-items-end">
-  {#each data.announcementLists as announcementList, index}
-    <a href={announcementList.url}><AnnounceBar message={announcementList.title} isDark={index !== 0} /></a>
-  {/each}
-</div>
+{#if announcementData.length > 0}
+  <div class="fixed grid right-0 gap-4 justify-items-end" in:fly={{ x: 200 }}>
+    {#each announcementData as announcementList, index}
+      <a href={announcementList.url} target="_blank" rel="noreferrer"><AnnounceBar message={announcementList.title} isDark={index !== 0} /></a>
+    {/each}
+  </div>
+{/if}
 <div class="flex flex-col w-full h-4/6">
   <div class="flex-grow" />
   <div
