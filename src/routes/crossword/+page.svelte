e1b9bc59c3c900e91fc83cce1a9cbcd5ec56f7dc
Plyrs1 <me@plyrs.eu.org>
2023-04-01 06:54:12 +0700

    fix: update crossword layout and clue (#36)
    * fix: update crossword layout and clue
    
    * fix: data not loaded on first page open
    
    * fix(ci): remove github actions in favor of vercel

--- src/routes/crossword/+page.svelte
+++ src/routes/crossword/+page.svelte
@@ -1,122 +1,84 @@
 <script lang="ts">
+  import { type Writable, writable } from 'svelte/store'
+
   import Crossword from '$src/lib/components/Crossword.svelte'
   import CrosswordClue from '$src/lib/components/CrosswordClue.svelte'
+  import Meta from '$src/lib/components/Meta.svelte'
 
-  const crosswordConfig = {
-    sizeX: 9, // Number of horizontal cells in the crossword
-    sizeY: 9, // Number of vertical cells in the crossword
-    question: [
-      {
-        number: 1,
-        x: 0,
-        y: 0,
-        length: 9,
-        direction: 'across',
-        desc: 'Clip, stroke, nut'
-      },
-      {
-        number: 5,
-        x: 0,
-        y: 2,
-        length: 4,
-        direction: 'across',
-        desc: 'Drive, brake, hard'
-      },
-      {
-        number: 7,
-        x: 5,
-        y: 2,
-        length: 4,
-        direction: 'across',
-        desc: 'Right, line, step'
-      },
-      {
-        number: 8,
-        x: 2,
-        y: 4,
-        length: 5,
-        direction: 'across',
-        desc: 'District, tennis, room'
-      },
-      {
-        number: 11,
-        x: 0,
-        y: 6,
-        length: 4,
-        direction: 'across',
-        desc: 'Crop, flow, ready'
-      },
-      {
-        number: 12,
-        x: 5,
-        y: 6,
-        length: 4,
-        direction: 'across',
-        desc: 'Break, number, money'
-      },
-      {
-        number: 13,
-        x: 0,
-        y: 8,
-        length: 9,
-        direction: 'across',
-        desc: 'Odd, seeker, buming'
-      },
-      {
-        number: 2,
-        x: 1,
-        y: 0,
-        length: 4,
-        direction: 'down',
-        desc: 'Price, home, trust'
-      },
-      {
-        number: 3,
-        x: 5,
-        y: 0,
-        length: 7,
-        direction: 'down',
-        desc: 'Bank, judgement, public'
-      },
-      {
-        number: 4,
-        x: 7,
-        y: 0,
-        length: 4,
-        direction: 'down',
-        desc: 'Killer, pink, like'
-      },
-      {
-        number: 6,
-        x: 3,
-        y: 2,
-        length: 7,
-        direction: 'down',
-        desc: 'Horse, evening, line'
-      },
-      {
-        number: 9,
-        x: 1,
-        y: 5,
-        length: 4,
-        direction: 'down',
-        desc: 'Wing, shoe, curtain'
-      },
-      {
-        number: 10,
-        x: 7,
-        y: 5,
-        length: 4,
-        direction: 'down',
-        desc: 'In, tolerance, hour'
-      }
-    ]
-  }
+  let currentQuestion: Writable<number> = writable(0)
+
+  const crosswordConfig = [
+    {
+      number: 1,
+      x: 15,
+      y: 0,
+      length: 23,
+      direction: 'across',
+      desc: 'The art of making an anime in a team'
+    },
+    {
+      number: 4,
+      x: 15,
+      y: 5,
+      length: 14,
+      direction: 'across',
+      desc: 'A rush-covered straw mat forming a traditional Japanese floor covering'
+    },
+    {
+      number: 5,
+      x: 4,
+      y: 7,
+      length: 17,
+      direction: 'across',
+      desc: 'Zombies in school'
+    },
+    {
+      number: 6,
+      x: 9,
+      y: 14,
+      length: 16,
+      direction: 'across',
+      desc: 'High magnitude earthquake in Tokyo'
+    },
+    {
+      number: 8,
+      x: 0,
+      y: 16,
+      length: 12,
+      direction: 'across',
+      desc: 'An orb is cast down to take shapes of whatever stimulates it'
+    },
+    {
+      number: 2,
+      x: 18,
+      y: 0,
+      length: 15,
+      direction: 'down',
+      desc: 'An assistant producer gets the chance of his life to direct a movie'
+    },
+    {
+      number: 3,
+      x: 25,
+      y: 3,
+      length: 8,
+      direction: 'down',
+      desc: 'Eating Potato chips dramatically'
+    },
+    {
+      number: 7,
+      x: 11,
+      y: 14,
+      length: 4,
+      direction: 'down',
+      desc: 'A man wakes up with no memories other than his name in a dystopian society'
+    }
+  ]
 </script>
 
+<Meta title="Crossword" description="I don't understand why these words are crossing each other..." />
 <div class="grid w-full place-items-center">
-  <div class="flex flex-col md:flex-row w-fit bg-kusogaki-purple p-8 gap-3">
-    <Crossword {crosswordConfig} />
-    <CrosswordClue {crosswordConfig} />
+  <div class="flex flex-col w-fit bg-kusogaki-purple p-2 sm:p-8 gap-3 font-lemon-milk">
+    <Crossword bind:currentQuestion {crosswordConfig} />
+    <CrosswordClue bind:currentQuestion {crosswordConfig} />
   </div>
 </div>
